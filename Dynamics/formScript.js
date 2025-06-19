a1e14b3599cac7b92e8c6c368119e64786a55509
Nisi-nice <frida333d999@gmail.com>
2025-06-19 05:35:16 -0600

    Añadí diseño, alerts y captcha

--- Dynamics/formScript.js
+++ Dynamics/formScript.js
@@ -1,32 +1,39 @@
-const form = document.querySelector('form');
-const button = document.querySelector('button');
-button.addEventListener('click', function(event) {
-  console.log(event);
-});
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  const nombre = document.getElementById("nombre").value;
-  const edad = document.getElementById("edad").value;
-  const codigoPostal = document.getElementById("cp").value;
-
-  if (nombre.length === 0 || edad.length === 0 || codigoPostal.length === 0) {
-    console.log("Dejaste algo sin responder :ccc");
-    return;
-  }
-
-  if (isNaN(edad)) {
-    console.log("La edad no es numérica :c");
-  } else {
-    console.log("La edad es numérica c:");
-  }
-
-  if (isNaN(codigoPostal)) {
-    console.log("El código postal no es numérico :00");
-  } else {
-    if (codigoPostal.length === 5) {
-      console.log("El CP tiene 5 dgitos");
-    } else {
-      console.log(`El CP tiene ${codigoPostal.length} dígitos.`);
+  const form = document.querySelector('form');
+  const button = document.querySelector('button');
+  const num1 = Math.floor(Math.random() * 10);
+  const num2 = Math.floor(Math.random() * 10);
+  const resultado = num1 + num2;
+  document.getElementById("pregunta").textContent = `${num1} + ${num2}`;
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    const nombre = document.getElementById("nombre").value;
+    const edad = document.getElementById("edad").value;
+    const codigoPostal = document.getElementById("cp").value;
+    const captchaValor = parseInt(document.getElementById("captcha").value, 10);
+//////////////validación/////////////
+    if (!nombre || !edad || !codigoPostal || isNaN(captchaValor)) {
+      alert("Dejaste algo sin responder :C");
+      return;
     }
-  }
-});
+    if (isNaN(edad)) {
+      alert("La edad no es numérica :c");
+      return;
+    }
+    if (isNaN(codigoPostal)) {
+      alert("El código postal no es numérico :00");
+      return;
+    }
+    if (codigoPostal.length !== 5) {
+      alert(`Escribiste ${codigoPostal.length} dígitos en el CP, deben ser 5.`);
+      return;
+    }
+/////////////captcha/////////////////
+    if (captchaValor !== resultado) {
+      alert("CAPTCHA incorrecto");
+      return;
+    }
+    alert("CAPTCHA verificado. Formulario enviado.");
+  });
+  button.addEventListener('click', function(event) {
+    console.log(event);
+  });
